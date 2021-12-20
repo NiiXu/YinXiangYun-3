6ee7cd00a9fbceb30aff688089161bc7340796e2
xu <chenhengxv@outlook.com>
2021-12-20 15:27:17 +0800

    引入axios封装统一接口

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -33,6 +33,14 @@
 </template>
 
 <script>
+
+import request from "../helper/request"
+
+request("/auth/login", "POST", {username: "hunger", password: "123456"})
+  .then(data => {
+    console.log(data)
+  })
+
 export default {
   data() {
     return {
